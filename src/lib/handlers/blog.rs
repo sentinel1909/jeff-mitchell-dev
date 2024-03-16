2bf015d7edc010b25e8aad856f110f3246fed142
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-03-16 09:54:10 -0700

    improvement: inject-dynamic-template-content

--- src/lib/handlers/blog.rs
+++ src/lib/handlers/blog.rs
@@ -5,5 +5,8 @@ use crate::templates::BlogTemplate;
 
 // index handler function
 pub async fn blog() -> BlogTemplate {
-    BlogTemplate {}
+    BlogTemplate {
+        header: "Latest Articles".to_string(),
+        content: "Here are all the things I've written...".to_string()
+    }
 }
--- src/lib/handlers/index.rs
+++ src/lib/handlers/index.rs
@@ -5,5 +5,8 @@ use crate::templates::IndexTemplate;
 
 // index handler function
 pub async fn index() -> IndexTemplate {
-    IndexTemplate {}
+    IndexTemplate {
+        header: "Welcome!".to_string(),
+        content: "This is my site.".to_string()
+    }
 }
