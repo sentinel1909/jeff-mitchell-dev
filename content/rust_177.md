dbf5ae03a62165818be51c7aae8ca96ead141827
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-03-21 20:31:59 -0700

    improvement: add real articles, add content to projects page

---
+++ content/rust_177.md
@@ -0,0 +1,13 @@
+---
+title: "Rust 1.77 Released"
+date: "2024-03-21"
+slug: "rust-177"
+category: "programming languages"
+tag: "rust"
+summary: "Rust 1.77 was released to the stable channel today"
+---
+
+# Rust 1.77 Released
+
+I can't believe it's been six weeks already. Today, Rust 1.77 dropped into the stable channel. Per usual, the details are in the official [Rust Blog post](https://blog.rust-lang.org/2024/03/21/Rust-1.77.0.html).
+
--- src/lib/queries/get_articles.rs
+++ src/lib/queries/get_articles.rs
@@ -7,7 +7,7 @@ use axum::extract::State;
 
 // function to get all articles from the database
 pub async fn get_articles(State(app_state): State<AppState>) -> Result<Vec<Article>, AppError> {
-    let articles = sqlx::query_as::<_, Article>("SELECT * FROM articles ORDER BY article_date ASC")
+    let articles = sqlx::query_as::<_, Article>("SELECT * FROM articles ORDER BY article_date DESC")
         .fetch_all(&app_state.pool)
         .await?;
     Ok(articles)
